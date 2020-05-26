997d93f1861bbbd6d570c3fc33e33349a7f2f7aa
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2020-05-26 21:28:11 +0300

    Can add new route

--- application/views.py
+++ application/views.py
@@ -1,6 +1,23 @@
 from flask import render_template, request
-from application import app
+from application import app, db
+from application.routes.models import Route
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/routes/new")
+def routes_form():
+  return render_template("routes/new.html")
+
+
+@app.route("/routes", methods=["POST"])
+def routes_create():
+  route_name = request.form.get("name")
+  route_type = request.form.get("routetype")
+  new_route = Route(route_name, route_type)
+  db.session().add(new_route)
+  db.session().commit()
+  return "Heloo pyworld"
