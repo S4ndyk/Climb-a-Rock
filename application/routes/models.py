ee932c450aa50f0b90a502e303dfcc9acf0a900d
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2020-05-26 21:01:55 +0300

    Initial project setup

---
+++ application/routes/models.py
@@ -0,0 +1,11 @@
+from application import db
+
+
+class Route(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100), nullable=False)
+  route_type = db.Column(db.String(100), nullable=False)
+
+  def __init__(self, name, route_type):
+    self.name = name
+    self.route_type = route_type
