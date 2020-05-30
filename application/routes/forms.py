33bde61a94efbba64075c7eca21d8aafdb71f751
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2020-05-30 11:46:15 +0300

    Add validators

--- application/routes/forms.py
+++ application/routes/forms.py
@@ -1,14 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField
+from wtforms import StringField, SelectField, validators
 
 class RouteForm(FlaskForm):
-  name = StringField("Route name")
-  route_type = SelectField('Type of route', choices=[(
-      'boulder', 'Boulder'), ('sport', 'Sport/Bolted'), ('traditional', 'Traditional')])
+  name = StringField("Route name", [validators.InputRequired(), validators.length(min=1, max=50)])
+  route_type = SelectField('Type of route', 
+  [validators.AnyOf(['boulder', 'sport', 'traditional'])],
+  choices=[('boulder', 'Boulder'), ('sport', 'Sport/Bolted'), ('traditional', 'Traditional')])
   class Meta:
     csrf = False
 
 class UpdateRouteForm(FlaskForm):
-  new_name = StringField('New name')
+  new_name = StringField('New name', [validators.InputRequired(), validators.length(min=1, max=50)])
   class Meta:
     csrf = False
